4306bf402a2c5ba2fdbb5f94ca7e99ab7b8b1269
rtrang <rtrang@uwaterloo.ca>
2021-11-13 18:42:02 -0500

    Syntax cleanup

--- laser_test.py
+++ laser_test.py
@@ -2,22 +2,23 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(15,GPIO.OUT);
+GPIO.setup(15, GPIO.OUT);
 
-pin = GPIO.PWM(15,500);
+pin = GPIO.PWM(15, 500);
 
 pin.start(0)
 
 try:
-   while 1:
-	
-      	br = input("Brightness (max 100)");
-      	if br>100:
-		br = 100;
-	if br<0:
-		br = 0;
+	while 1:
+		br = input("Brightness (max 100)")
 
-        pin.ChangeDutyCycle(br);
+		if br > 100:
+			br = 100
+		if br < 0:
+			br = 0
+
+		pin.ChangeDutyCycle(br);
+		
 except KeyboardInterrupt:
    pass
 
